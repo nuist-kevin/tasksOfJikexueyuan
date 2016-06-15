6e414168fa84ec152e740213442f4175229663ff
nuist-kevin <nuist_kevin@163.com>
2016-06-15 17:19:46 +0800

    添加三角函数等操作

--- task2/js/calculator.js
+++ task2/js/calculator.js
@@ -10,15 +10,15 @@ var operand1 = '0';
 var operand2 = '';
 var operator = '';
 var result = '0';
-var phase = 0;
+var phase = 1;
 
 //为所有按钮添加鼠标悬停效果
 var buttons = document.querySelectorAll(".btn");
 for (var index = 0; index < buttons.length; index++) {
-    buttons[index].addEventListener('mouseover', function () {
+    buttons[index].addEventListener('mouseover', function() {
         this.classList.add('buttonOn');
     });
-    buttons[index].addEventListener('mouseout', function () {
+    buttons[index].addEventListener('mouseout', function() {
         this.classList.remove('buttonOn');
     });
 
@@ -27,30 +27,36 @@ for (var index = 0; index < buttons.length; index++) {
 //数字按钮
 var numBtns = document.querySelectorAll(".num");
 for (var idx = 0; idx < numBtns.length; idx++) {
-    numBtns[idx].onclick = function () {
-        if (phase == 0 || phase == 1) {
-            phase = 1;
+    numBtns[idx].onclick = function() {
+        //1阶段时，区分初始为0的情况和持续输入的情况
+        if (phase == 1) {
+            //初始为0时直接赋值
             if (operand1 == '0') {
                 operand1 = this.value;
             } else {
+                //持续输入时拼接字符串
                 operand1 += this.value;
             }
             result = operand1;
         }
         if (phase == 2 || phase == 3) {
+            //阶段2和阶段3输入数字，先进入阶段3
             phase = 3;
-            if (operand1 == '0') {
+            //初始为0时直接赋值，持续输入时拼接字符串
+            if (operand2 == '0') {
                 operand2 = this.value;
             } else {
                 operand2 += this.value;
             }
             result = operand2;
         }
+        //在4阶段已经计算结束时输入数字，直接进入阶段1
         if (phase == 4) {
             phase = 1;
-            result = '';
             operand2 = '';
             operand1 = this.value;
+            result = operand1;
+
         }
         document.querySelector("#result").value = result;
         document.getElementById("typeRecord").value = parseFloat(operand1).toString() + ' ' + operator + ' ' + (operand2 ? parseFloat(operand2).toString() : '');
@@ -59,31 +65,29 @@ for (var idx = 0; idx < numBtns.length; idx++) {
 }
 
 //小数点按钮
-document.querySelector(".dot").onclick = function () {
+document.querySelector(".dot").onclick = function() {
     switch (phase) {
-        //兼容应该输入数字时直接点击小数点按钮，此时默认为小数点前加上0
-        case 0:
-            phase = 1;
-            operand1 = '0.';
-            result = operand1;
-            break;
+        //在1阶段输入小数点，首先判断是否包含小数点，不包含的情况下再做拼接   
         case 1:
             if (!operand1.match("\\.")) {
                 operand1 += '.';
                 result = operand1;
             }
             break;
+            //在2阶段输入小数点，由于此时没有输入操作数2，直接认为操作数2以'0.'开头，并进入阶段3
         case 2:
             phase = 3;
             operand2 = '0.';
             result = operand2
             break;
+            //在3阶段输入小数点，首先判断是否包含小数点，不包含的情况下再做拼接   
         case 3:
             if (!operand2.match("\\.")) {
                 operand2 += '.';
                 result = operand2;
             }
             break;
+            //在4阶段已经计算结束时输入小数点，直接进入阶段1，操作数1以'0.'开头
         case 4:
             phase = 1;
             operand2 = '';
@@ -98,7 +102,7 @@ document.querySelector(".dot").onclick = function () {
 //操作符按钮
 var operBtns = document.querySelectorAll(".operator");
 for (var i = 0; i < operBtns.length; i++) {
-    operBtns[i].onclick = function () {
+    operBtns[i].onclick = function() {
         if (phase != 0) {
             if (phase == 3) {
                 calculate();
@@ -118,13 +122,11 @@ for (var i = 0; i < operBtns.length; i++) {
 }
 
 //退格按钮
-document.querySelector(".del").onclick = function () {
+document.querySelector(".del").onclick = function() {
     switch (phase) {
-        case 0:
-
         case 1:
             if (operand1.length >= 2) {
-            operand1 = operand1.slice(0, -1);
+                operand1 = operand1.slice(0, -1);
             } else {
                 operand1 = '0';
             }
@@ -149,18 +151,18 @@ document.querySelector(".del").onclick = function () {
 }
 
 //清除按钮
-document.querySelector(".clear").onclick = function () {
-    operand1 = '';
+document.querySelector(".clear").onclick = function() {
+    operand1 = '0';
     operand2 = '';
     operator = '';
     result = '';
     document.querySelector("#result").value = '0';
     document.getElementById("typeRecord").value = '0';
-    phase = 0;
+    phase = 1;
 }
 
 //等于按钮
-document.querySelector(".equal").onclick = function () {
+document.querySelector(".equal").onclick = function() {
     if (phase != 0 && phase != 1) {
         if (phase == 2) {
             operand2 = operand1;
@@ -173,69 +175,100 @@ document.querySelector(".equal").onclick = function () {
     }
 }
 
-function calculate() {
-    if (operator == '-') {
-        result = floatMinus(operand1, operand2);
-    }
-    if (operator == '+') {
-        result = floatAdd(operand1, operand2);
-    }
-    if (operator == '*') {
-        result = floatMultiple(operand1, operand2);
-    }
-    if (operator == '/') {
-        if (parseInt(operand2) == 0) {
-            result = '除数不能为零'
-        } else {
-            result = floatDivide(operand1, operand2);
+//一元操作符
+var operBtns = document.querySelectorAll(".oper");
+for (var index = 0; index < operBtns.length; index++) {
+    operBtns[index].addEventListener("click", function() {
+        switch (phase) {
+            case 1:
+                result = eval('Math.' + this.value + '(operand1)');
+                document.getElementById("typeRecord").value = this.value + '(' + operand1 + ')';
+                break;
+            case 3:
+                document.getElementById("typeRecord").value = operand1 + operator + this.value + '(' + operand2 + ')';
+                operand2 = eval('Math.' + this.value + '(operand2)');
+                calculate();
+                break;
+            case 4:
+                operand1 = result;
+                operand2 = '';
+                result = eval('Math.' + this.value + '(operand1)');
+                document.getElementById("typeRecord").value = this.value + '(' + operand1 + ')';
         }
-    }
-    if (operator == '%') {
-        if (parseInt(operand2) == 0) {
-            result = '不能模零'
-        } else {
-            result = floatMod(operand1, operand2);
+        phase = 4;
+        document.querySelector("#result").value = result;
+    });
+}
+
+    function calculate() {
+        if (operator == '-') {
+            result = floatMinus(operand1, operand2);
         }
+        if (operator == '+') {
+            result = floatAdd(operand1, operand2);
+        }
+        if (operator == '*') {
+            result = floatMultiple(operand1, operand2);
+        }
+        if (operator == '/') {
+            if (parseInt(operand2) == 0) {
+                result = '除数不能为零'
+            } else {
+                result = floatDivide(operand1, operand2);
+            }
+        }
+        if (operator == '%') {
+            if (parseInt(operand2) == 0) {
+                result = '不能模零'
+            } else {
+                result = floatMod(operand1, operand2);
+            }
+        }
+
+        document.querySelector("#result").value = result;
+
     }
 
-    document.querySelector("#result").value = result;
 
-}
+    /**** ********************************************************************************
+     ***** 重写浮点数四则运算，解决不精确问题，如0.1 + 02 不等于0.3
+     ***** 先取出两个操作数小数点后最长的位数，如4位，再将两个操作数乘以10的4次方，以此全部转化为整数
+     ***** 对转化后的整数做四则运算，得出结果后再除以10的4次方。
+     ***** ********************************************************************************/
+    function getMultiple(str1, str2) {
+        if(typeof(str1) !== 'string') {
+            str1 = str1.toString();
+        }
+        if(typeof(str2) !== 'string') {
+            str2 = str2.toString();
+        }
+        var pointlength1 = str1.split(".")[1] ? str1.split(".")[1].length : 0;
+        var pointlength2 = str2.split(".")[1] ? str2.split(".")[1].length : 0;
+        var power = (pointlength1 >= pointlength2) ? pointlength1 : pointlength2;
+        return Math.pow(10, power);
+    }
 
+    function floatAdd(operand1, operand2) {
+        var multiple = getMultiple(operand1, operand2);
+        return (multiple * parseFloat(operand1) + multiple * parseFloat(operand2)) / multiple;
+    }
 
-/**** ********************************************************************************
- ***** 重写浮点数四则运算，解决不精确问题，如0.1 + 02 不等于0.3
- ***** 先取出两个操作数小数点后最长的位数，如4位，再将两个操作数乘以10的4次方，以此全部转化为整数
- ***** 对转化后的整数做四则运算，得出结果后再除以10的4次方。
- ***** ********************************************************************************/
-function getMultiple(str1, str2) {
-    var pointlength1 = str1.split(".")[1] ? str1.split(".")[1].length : 0;
-    var pointlength2 = str2.split(".")[1] ? str2.split(".")[1].length : 0;
-    var power = (pointlength1 >= pointlength2) ? pointlength1 : pointlength2;
-    return Math.pow(10, power);
-}
+    function floatMinus(operand1, operand2) {
+        var multiple = getMultiple(operand1, operand2);
+        return (multiple * parseFloat(operand1) - multiple * parseFloat(operand2)) / multiple;
+    }
 
-function floatAdd(operand1, operand2) {
-    var multiple = getMultiple(operand1, operand2);
-    return (multiple * parseFloat(operand1) + multiple * parseFloat(operand2)) / multiple;
-}
+    function floatMultiple(operand1, operand2) {
+        var multiple = getMultiple(operand1, operand2);
+        return (multiple * parseFloat(operand1) * (multiple * parseFloat(operand2))) / (multiple * multiple);
+    }
 
-function floatMinus(operand1, operand2) {
-    var multiple = getMultiple(operand1, operand2);
-    return (multiple * parseFloat(operand1) - multiple * parseFloat(operand2)) / multiple;
-}
+    function floatDivide(operand1, operand2) {
+        var multiple = getMultiple(operand1, operand2);
+        return multiple * parseFloat(operand1) / (multiple * parseFloat(operand2));
+    }
 
-function floatMultiple(operand1, operand2) {
-    var multiple = getMultiple(operand1, operand2);
-    return (multiple * parseFloat(operand1) * (multiple * parseFloat(operand2))) / (multiple * multiple);
-}
-
-function floatDivide(operand1, operand2) {
-    var multiple = getMultiple(operand1, operand2);
-    return multiple * parseFloat(operand1) / (multiple * parseFloat(operand2));
-}
-
-function floatMod(operand1, operand2) {
-    var multiple = getMultiple(operand1, operand2);
-    return (multiple * parseFloat(operand1) % (multiple * parseFloat(operand2))) / multiple;
-}
+    function floatMod(operand1, operand2) {
+        var multiple = getMultiple(operand1, operand2);
+        return (multiple * parseFloat(operand1) % (multiple * parseFloat(operand2))) / multiple;
+    }
